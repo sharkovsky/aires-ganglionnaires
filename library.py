48e707de2bdef97e2ea547c3ca929d5f655bbe58
Francesco Cremonesi <francesco.cremonesi0@gmail.com>
2025-01-03 15:14:52 +0100

    Add option to process each z slice independently

--- library.py
+++ library.py
@@ -2,7 +2,7 @@ import numpy as np
 import nibabel as nib
 from skimage.morphology import label
 import os
-from typing import List, Dict
+from typing import List, Dict, Optional
 from functools import reduce
 
 
@@ -54,11 +54,20 @@ def get_extremal_idx(organ_segmentation: 'ImageSegmentation', axis: int, get_lar
 
 def get_extremal_idx_by_z_slice(organ_segmentation: 'ImageSegmentation', axis: int, get_largest_index: bool = True) -> List[int]:
     projection = organ_segmentation.sum(1-axis)
-    pixel_indices = np.max(np.where(projection > 0)[0]) if get_largest_index else np.min(np.where(projection > 0)[0])
-    return pixel_indices
+    axis_indices = list()
+    z_indices = np.unique(np.where(projection > 0)[1])
+    for z in z_indices:
+        axis_indices.append(
+            np.max(np.where(projection[...,z] > 0)[0]) if get_largest_index else np.min(np.where(projection[...,z] > 0)[0])
+        )
+    return np.array(axis_indices), z_indices
 
 
-def define_area_by_plane(organ_segmentation: 'ImageSegmentation', axis: int, get_largest_index: bool = True, one_after: bool = True) -> 'ImageSegmentation':
+def define_area_by_plane(organ_segmentation: 'ImageSegmentation',
+                         axis: int,
+                         get_largest_index: bool = True,
+                         one_after: bool = True,
+                         slice_by_slice: bool = False) -> 'ImageSegmentation':
     """Returns a new mask that defines a 3d-rectangular area in the image.
 
     The new mask is split in two areas, divided by the plane perpendicular to `axis` at the position 
@@ -71,27 +80,43 @@ def define_area_by_plane(organ_segmentation: 'ImageSegmentation', axis: int, get
         organ_segmentation: the binary mask representing the segmentation of an organ
         axis: the integer corresponding to the axis where we want to find the extreme point (e.g. 2 for z axis)
         get_largest_index: boolean to get the largest or the smallest index on the projection
-        one_after.
+        one_after: boolean as explained above
+        slice_by_slice: boolean to toggle processing each z-slice separately (True) or combined (False, default)
 
     Returns:
         area_mask: a binary image with the same shape as mask 
 
     """
-    idx = get_extremal_idx(organ_segmentation, axis, get_largest_index)
-    area_mask = np.zeros_like(organ_segmentation)
-    selecting_slice = slice(idx, None) if one_after else slice(None, idx)
-    match axis:
-        case 0:
-            indices = (selecting_slice, Ellipsis)
-        case 1:
-            indices = (slice(None), selecting_slice, slice(None))
-        case 2:
-            indices = (Ellipsis, selecting_slice)
-    area_mask[indices] = 1
+    if slice_by_slice:
+        area_mask = np.zeros_like(organ_segmentation)
+        axis_indices, z_indices = get_extremal_idx_by_z_slice(organ_segmentation, axis, get_largest_index)
+        for i in range(z_indices):
+            selecting_slice = slice(axis_indices[i], None) if one_after else slice(None, axis_indices[i])
+            match axis:
+                case 0:
+                    indices = (selecting_slice, slice(None), z_indices[i])
+                case 1:
+                    indices = (slice(None), selecting_slice, z_indices[i])
+            area_mask[indices] = 1
+    else:
+        idx = get_extremal_idx(organ_segmentation, axis, get_largest_index)
+        area_mask = np.zeros_like(organ_segmentation)
+        selecting_slice = slice(idx, None) if one_after else slice(None, idx)
+        match axis:
+            case 0:
+                indices = (selecting_slice, Ellipsis)
+            case 1:
+                indices = (slice(None), selecting_slice, slice(None))
+            case 2:
+                indices = (Ellipsis, selecting_slice)
+        area_mask[indices] = 1
     return area_mask
 
 
-def define_area_by_specs(area_specs: Dict[str, List[Dict]], patient: str, path_to_totalseg_segmentations: str, totalseg_structure_to_task) -> 'ImageSegmentation':
+def define_area_by_specs(area_specs: Dict[str, List[Dict]],
+        patient: str,
+        path_to_totalseg_segmentations: str,
+        totalseg_structure_to_task) -> 'ImageSegmentation':
     """Returns a new mask that defines an aire ganglionnaire based on a dictionary of specifications.
 
     The specifications explain how each border of the aire ganglionnaire is defined. 
@@ -131,6 +156,79 @@ def define_area_by_specs(area_specs: Dict[str, List[Dict]], patient: str, path_t
                     )
                     organ_segmentation = nib.load(seg_file_path).get_fdata()
                 yield define_area_by_plane(organ_segmentation, axis_,get_largest_index_, one_after_)
+    return reduce(lambda x,y: x*y, _gen_areas())
+
+
+def define_area_by_specs_with_heuristics(area_specs: Dict[str, List[Dict]],
+        patient: str,
+        path_to_totalseg_segmentations: str,
+        totalseg_structure_to_task) -> 'ImageSegmentation':
+    """Returns a new mask that defines an aire ganglionnaire based on a dictionary of specifications.
+
+    Implements some heuristics:
+    - first define superior and inferior borders
+    - for x and y borders, only search for extrema *within the top and bottom slice*
+    - for x and y borders, search for extrema slice-by-slice instead of globally
+
+    The specifications explain how each border of the aire ganglionnaire is defined. 
+    The format of the specs is:
+    ```
+    {border of the aire ganglionnaire: [structure_specs]}
+    ```
+    Each `structure_spec` is itself a dictionary with the keys:
+    - structure: totalsegmentator filename
+    - border: which border of the totalsegmentator class defines the border of the aire ganglionnaire
+
+    Example:
+    ```
+    level_iia_left_specs = {
+        'inferior border': [{'border': ['inferior border'], 'structure': 'hyoid'}],
+        'superior border': [{'border': ['superior border'], 'structure': 'vertebrae_C1'}],
+        'posterior border': [{'border': ['posterior border'], 'structure': 'internal_jugular_vein_left'}],
+        'anterior border': [{'border': ['posterior border'], 'structure': 'submandibular_gland_left'}],
+        'left border': [{'border': ['left border'], 'structure': 'sternocleidomastoid_left'}],
+        'right border': [{'border': ['right border'], 'structure': 'internal_carotid_artery_left'}],
+    }
+```
+"""
+    do_first_borders =  ['superior border', 'inferior border']
+    slice_by_slice_ = True
+    def _gen_areas():
+        for border in do_first_borders:
+            specs = area_specs[border]
+            one_after_ = specs_to_args[border]['one_after']
+            axis_ = specs_to_args[border]['axis']
+            for organ_border_specs in specs:
+                seg_filename = organ_border_specs['structure'] + '.nii.gz'
+                for organ_border_name in organ_border_specs['border']:
+                    get_largest_index_ = specs_to_args[organ_border_name]['get_largest_index']
+                    seg_file_path = os.path.join(
+                        path_to_totalseg_segmentations,
+                        totalseg_structure_to_task[seg_filename],
+                        patient,
+                        seg_filename
+                    )
+                    organ_segmentation = nib.load(seg_file_path).get_fdata()
+                yield define_area_by_plane(organ_segmentation, axis_, get_largest_index_, one_after_, slice_by_slice_)
+ 
+        # do the other borders
+        for border, specs in area_specs.items():
+            if border in do_first_borders:
+                continue
+            one_after_ = specs_to_args[border]['one_after']
+            axis_ = specs_to_args[border]['axis']
+            for organ_border_specs in specs:
+                seg_filename = organ_border_specs['structure'] + '.nii.gz'
+                for organ_border_name in organ_border_specs['border']:
+                    get_largest_index_ = specs_to_args[organ_border_name]['get_largest_index']
+                    seg_file_path = os.path.join(
+                        path_to_totalseg_segmentations,
+                        totalseg_structure_to_task[seg_filename],
+                        patient,
+                        seg_filename
+                    )
+                    organ_segmentation = nib.load(seg_file_path).get_fdata()
+                yield define_area_by_plane(organ_segmentation, axis_, get_largest_index_, one_after_, slice_by_slice_)
     return reduce(lambda x,y: x*y, _gen_areas())
 
 
