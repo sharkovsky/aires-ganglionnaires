92abf51382a2c7db63ae9a03c3a1f3c8a6f95c74
Francesco Cremonesi <francesco.cremonesi@inria.fr>
2025-08-04 09:20:22 +0200

    Fix wrong specs level iv

--- lymph_node_levels_specs.py
+++ lymph_node_levels_specs.py
@@ -60,7 +60,7 @@ level_specs = {
         'inferior border': [{'border': ['superior point'], 'structure': 'sternum'}], #pixel le plus haut
         'superior border': [{'border': ['inferior point'], 'structure': 'cricoid_cartilage'}], #pixel le plus bas
         'posterior border': [{'border': ['posterior border'], 'structure': 'anterior_scalene_left'}],
-        'anterior border': [{'border': ['posterior point'], 'structure': 'sternocleidomastoid_left'}],
+        'anterior border': [{'border': ['anterior point'], 'structure': 'sternocleidomastoid_left'}],
         'left border': [{'border': ['left border'], 'structure': 'anterior_scalene_left'}],
         'right border': [{'border': ['right border'], 'structure': 'common_carotid_artery_left'}] #coupe axiale de son pixel le plus à droite 
         },
@@ -68,7 +68,7 @@ level_specs = {
         'inferior border': [{'border': ['superior point'], 'structure': 'sternum'}], #pixel le plus haut
         'superior border': [{'border': ['inferior point'], 'structure': 'cricoid_cartilage'}], #pixel le plus bas
         'posterior border': [{'border': ['posterior border'], 'structure': 'anterior_scalene_right'}],
-        'anterior border': [{'border': ['posterior point'], 'structure': 'sternocleidomastoid_right'}],
+        'anterior border': [{'border': ['anterior point'], 'structure': 'sternocleidomastoid_right'}],
         'left border': [{'border': ['left border'], 'structure': 'common_carotid_artery_right'}], #coupe axiale de son pixel le plus à gauche
         'right border': [{'border': ['right border'], 'structure': 'anterior_scalene_right'}]
         },
